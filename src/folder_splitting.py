64137f9f7941dabc782445da6ee5b01409dd0519
MTPrime <michael@tylerclan.com>
2019-10-15 10:26:39 -0600

    Folders split into train, test, val

--- src/folder_splitting.py
+++ src/folder_splitting.py
@@ -1,1 +1,5 @@
 import split_folders
+
+# Split with a ratio.
+# To only split into training and validation set, set a tuple to `ratio`, i.e, `(.8, .2)`.
+split_folders.ratio('/Users/mt/Galvanize/capstones/street_art_classifier-/data/img', output="/Users/mt/Galvanize/capstones/street_art_classifier-/data/train_test_split", seed=42, ratio=(.64, .16, .2))
