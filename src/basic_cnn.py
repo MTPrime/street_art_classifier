c0eac49e104de08e74c8868cf84bef80b21a2bc2
MTPrime <michael@tylerclan.com>
2019-10-15 10:29:48 -0600

    Commented out RAdam for EC2

--- src/basic_cnn.py
+++ src/basic_cnn.py
@@ -10,7 +10,7 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten
 from tensorflow.keras.layers import Conv2D, MaxPooling2D
 from tensorflow.keras.utils import to_categorical
-from keras_radam import RAdam
+# from keras_radam import RAdam
 from keras.preprocessing.image import ImageDataGenerator
 
 def load_and_featurize_data():
