bd67306db57cabe13553efb5fbda302e2cfbf075
MTPrime <michael@tylerclan.com>
2019-10-17 21:24:36 -0600

    6 model for ec2

--- src/street_art_cnn.py
+++ src/street_art_cnn.py
@@ -6,6 +6,7 @@ from tensorflow.keras.utils import to_categorical
 # from keras_radam import RAdam
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.models import load_model
+from tensorflow.keras.callbacks import ModelCheckpoint
 
 def create_data_generators(directory_path='data/train_test_split/', input_shape=(64, 64), batch_size=16):
     
@@ -13,16 +14,6 @@ def create_data_generators(directory_path='data/train_test_split/', input_shape=
     test_path = directory_path +'test'
     val_path = directory_path +'val'
 
-    # train_datagen = ImageDataGenerator(
-    #     rescale=1./255,
-    #     rotation_range=6,
-    #     width_shift_range=0.1,
-    #     height_shift_range=0.1,
-    #     brightness_range=[0.2, 0.8],
-    #     shear_range=0.2,
-    #     zoom_range=0.2,
-    #     horizontal_flip=True
-    #     )
 
     train_datagen = ImageDataGenerator(
             rescale=1./255,
@@ -38,19 +29,22 @@ def create_data_generators(directory_path='data/train_test_split/', input_shape=
         train_path,
         target_size=input_shape,
         batch_size=batch_size,
-        class_mode='categorical')
+        class_mode='categorical',
+        shuffle=False)
         
     val_generator = test_datagen.flow_from_directory(
         val_path,
         target_size=input_shape,
         batch_size=batch_size,
-        class_mode='categorical')
+        class_mode='categorical',
+        shuffle=False)
 
     test_generator = test_datagen.flow_from_directory(
         test_path,
         target_size=input_shape,
         batch_size=batch_size,
-        class_mode='categorical')
+        class_mode='categorical',
+        shuffle=False)
 
     return train_generator, test_generator, val_generator
 
@@ -71,7 +65,11 @@ def build_model(opt='adam', input_shape=(64, 64, 3), nb_classes = 5, neurons = 6
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=pool_size, name='pool-2'))
 
-    
+    model.add(Conv2D(nb_filters, 
+                    (kernel_size[0], kernel_size[1]), 
+                    name='conv-3')) 
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=pool_size, name='pool-3'))
 
     model.add(Flatten())  # this converts our 3D feature maps to 1D feature vectors
     model.add(Dense(neurons))
@@ -90,15 +88,15 @@ def build_model(opt='adam', input_shape=(64, 64, 3), nb_classes = 5, neurons = 6
     return model
 
 if __name__ == '__main__':
-    batch_size = 10  
-    nb_classes = 2   
-    nb_epoch = 1    
-    img_rows, img_cols = 150, 150  
+    batch_size = 20  
+    nb_classes = 6   
+    nb_epoch = 5              
+    img_rows, img_cols = 64, 64  
     input_shape = (img_rows, img_cols, 3)  
     nb_filters = 12  
     pool_size = (2, 2)
     kernel_size = (3, 3) 
-    neurons=64
+    neurons=128
 
     train_generator, test_generator, val_generator = create_data_generators(directory_path='data/train_test_split/', 
                                                                             input_shape=(img_rows,img_cols), 
@@ -111,14 +109,23 @@ if __name__ == '__main__':
                             nb_filters = nb_filters, 
                             pool_size = pool_size, 
                             kernel_size = kernel_size)
+
+    art_model.load_weights('./models/street_art_cnn_weights_86.h5', by_name=True)
+    # art_model = load_model('./models/street_art_cnn.h5')
+    
+    #Checkpoint
+    filepath="weights-improvement-{epoch:02d}-{val_accuracy:.2f}.hdf5"
+    checkpoint = ModelCheckpoint(filepath, monitor='val_acc', mode='max', save_weights_only=True, save_best_only=True, period=1)
+    callbacks_list = [checkpoint]
+
     
     art_model.fit_generator(
             train_generator,
-            steps_per_epoch=1000,
+            steps_per_epoch=2000,
             epochs=nb_epoch,
             validation_data=val_generator,
-            validation_steps=800//batch_size,
+            validation_steps=200,
             use_multiprocessing=True)
     
-    art_model.save_weights('street_art_cnn.h5')
-    art_model.save('street_art_cnn.h5')
+    art_model.save_weights('6_class_weights.h5')
+    art_model.save('6_class_model.h5')
