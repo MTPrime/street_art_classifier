a3a9d75e1224428132b6d1ca7826f665822f974b
MTPrime <michael@tylerclan.com>
2019-11-05 13:16:38 -0700

    Added tensorboard to main CNN

--- src/street_art_cnn.py
+++ src/street_art_cnn.py
@@ -1,4 +1,5 @@
 from tensorflow import keras
+from tensorflow.keras import callbacks
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten
 from tensorflow.keras.layers import Conv2D, MaxPooling2D
@@ -7,6 +8,9 @@ from tensorflow.keras.utils import to_categorical
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.models import load_model
 from tensorflow.keras.callbacks import ModelCheckpoint
+from tensorflow.keras.metrics import Precision, Recall 
+import matplotlib.pyplot as plt
+import argparse
 
 def create_data_generators(directory_path='data/train_test_split/', input_shape=(64, 64), batch_size=16):
     
@@ -71,7 +75,7 @@ def build_model(opt='adam', input_shape=(64, 64, 3), nb_classes = 5, neurons = 6
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=pool_size, name='pool-3'))
 
-    model.add(Flatten())  # this converts our 3D feature maps to 1D feature vectors
+    model.add(Flatten())  
     model.add(Dense(neurons))
     model.add(Activation('relu'))
     model.add(Dropout(0.5))
@@ -80,19 +84,51 @@ def build_model(opt='adam', input_shape=(64, 64, 3), nb_classes = 5, neurons = 6
         model.add(Activation('sigmoid'))
     else:
         model.add(Activation('softmax'))
-
+    
+        
     model.compile(loss='categorical_crossentropy',
                 optimizer=opt,
                 metrics=['accuracy'])
-    
+
     return model
+
+def graph_loss(history, epochs):
+    #Graphing
+    # plot loss during training
+    fig, ax = plt.subplots(4, figsize = (12, 8))
+    ax[0].set_title('Loss')
+    ax[0].set_xticks(range(0,epochs+1,5))
+    ax[0].plot(history.history['loss'], label='train')
+    ax[0].plot(history.history['val_loss'], label='test')
+    ax[0].legend()
+
+    # plot accuracy during training
+    ax[1].set_xticks(range(0,epochs+1,5))
+    ax[1].set_title('Accuracy')
+    ax[1].plot(history.history['accuracy'], label='train')
+    ax[1].plot(history.history['val_accuracy'], label='test')
+    ax[1].legend()
+
+    ax[2].set_xticks(range(0,epochs+1,5))
+    ax[2].set_title('Precision')
+    ax[2].plot(history.history['precision_1'], label='train')
+    ax[2].plot(history.history['val_precision_1'], label='test')
+    ax[2].legend()
+
+    ax[3].set_xticks(range(0,epochs+1,5))
+    ax[3].set_title('Recall')
+    ax[3].plot(history.history['recall_1'], label='train')
+    ax[3].plot(history.history['val_recall_1'], label='test')
+    ax[3].legend()
+
+    plt.savefig('images/model_loss.png')
 
 if __name__ == '__main__':
     #Settings
     batch_size = 20  
     nb_classes = 6   
-    nb_epoch = 50              
-    img_rows, img_cols = 64, 64  
+    nb_epoch = 3              
+    img_rows, img_cols = 100, 100  
     input_shape = (img_rows, img_cols, 3)  
     nb_filters = 12  
     pool_size = (2, 2)
@@ -103,7 +139,7 @@ if __name__ == '__main__':
                                                                             input_shape=(img_rows,img_cols), 
                                                                             batch_size=batch_size)
 
-    art_model = build_model(opt='adam', 
+    model = build_model(opt='adam', 
                             input_shape=input_shape, 
                             nb_classes = nb_classes, 
                             neurons = neurons, 
@@ -111,6 +147,34 @@ if __name__ == '__main__':
                             pool_size = pool_size, 
                             kernel_size = kernel_size)
 
+
+    tensorboard = callbacks.TensorBoard(
+        log_dir='logdir',
+        histogram_freq=0, 
+        write_graph=True,
+        write_grads=True, 
+        update_freq='epoch')
+
+
+    savename = "{0}_best.h5".format('6_class_model')
+
+
+    mc = callbacks.ModelCheckpoint(
+        savename,
+        monitor='val_acc', 
+        verbose=0, 
+        save_best_only=True, 
+        mode='auto', 
+        save_freq='epoch')
+
+    history = model.fit_generator(train_generator,
+                                      steps_per_epoch=2000/batch_size,
+                                      epochs=nb_epoch,
+                                      validation_data=val_generator,
+                                      validation_steps=200/batch_size,
+                                      callbacks=[mc, tensorboard])
+    # callbacks = [mc, tensorboard]
+    graph_loss(history, nb_epoch)
     
     # art_model.load_weights('6_class_weights.h5', by_name=True)
     # art_model = load_model('./models/street_art_cnn_weights_86.h5')
@@ -121,13 +185,13 @@ if __name__ == '__main__':
     # callbacks_list = [checkpoint]
 
     
-    art_model.fit_generator(
-            train_generator,
-            steps_per_epoch=2000,
-            epochs=nb_epoch,
-            validation_data=val_generator,
-            validation_steps=200,
-            use_multiprocessing=True)
+    # art_model.fit_generator(
+    #         train_generator,
+    #         steps_per_epoch=2000//batch_size,
+    #         epochs=nb_epoch,
+    #         validation_data=val_generator,
+    #         validation_steps=200//batch_size,
+    #         use_multiprocessing=True)
     
-    art_model.save_weights('6_class_weights_2.h5')
-    art_model.save('6_class_model_2.h5')
+    model.save_weights('6_class_weights_test.h5')
+    model.save('6_class_model_test.h5')
