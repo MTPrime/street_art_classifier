6562612e4902c1abc755b791e784818fffb8891e
MTPrime <michael@tylerclan.com>
2019-11-07 11:09:41 -0700

    Changed architecture so that input and output matched.

--- src/convolutional_autoencoder.py
+++ src/convolutional_autoencoder.py
@@ -59,16 +59,14 @@ def autoencoder(input_img):
     #input = 28 x 28 x 1 (wide and thin)
     conv1 = Conv2D(32, (3, 3), activation='relu', padding='same')(input_img) #28 x 28 x 32
     pool1 = MaxPooling2D(pool_size=(2, 2))(conv1) #14 x 14 x 32
-    conv2 = Conv2D(16, (3, 3), activation='relu', padding='same')(pool1) #14 x 14 x 64
-    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2) #7 x 7 x 64
-    conv3 = Conv2D(8, (3, 3), activation='relu', padding='same')(pool2) #7 x 7 x 128 (small and thick)
-    code_layer = MaxPooling2D(pool_size=(2, 2), padding='same')(conv3)
-
+    conv2 = Conv2D(64, (3, 3), activation='relu', padding='same')(pool1) #14 x 14 x 64
+    code_layer = MaxPooling2D(pool_size=(2, 2))(conv2) #7 x 7 x 64
+    
     #decoder
-    conv4 = Conv2D(8, (3, 3), activation='relu', padding='same')(code_layer) #7 x 7 x 128
-    up1 = UpSampling2D((2,2))(conv4) # 14 x 14 x 128
-    conv5 = Conv2D(16, (3, 3), activation='relu', padding='same')(up1) # 14 x 14 x 64
-    up2 = UpSampling2D((2,2))(conv5) # 28 x 28 x 64
+    conv3 = Conv2D(128, (3, 3), activation='relu', padding='same')(code_layer) #7 x 7 x 128
+    up1 = UpSampling2D((2,2))(conv3) # 14 x 14 x 128
+    conv4 = Conv2D(64, (3, 3), activation='relu', padding='same')(up1) # 14 x 14 x 64
+    up2 = UpSampling2D((2,2))(conv4) # 28 x 28 x 64
     decoded = Conv2D(3, (3, 3), activation='sigmoid', padding='same')(up2) # 28 x 28 x 1
     return decoded
 
@@ -94,28 +92,28 @@ if __name__ == '__main__':
 
     print(autoencoder.summary())
 
-    tensorboard = callbacks.TensorBoard(
-        log_dir='logdir',
-        histogram_freq=0, 
-        write_graph=True,
-        update_freq='epoch')
+    # tensorboard = callbacks.TensorBoard(
+    #     log_dir='logdir',
+    #     histogram_freq=0, 
+    #     write_graph=True,
+    #     update_freq='epoch')
     
-    savename = 'best_autoencoder_model.h5'
+    # savename = 'best_autoencoder_model.h5'
 
-    mc = callbacks.ModelCheckpoint(
-        savename,
-        monitor='val_accuracy', 
-        verbose=0, 
-        save_best_only=True, 
-        mode='auto', 
-        save_freq='epoch')
+    # mc = callbacks.ModelCheckpoint(
+    #     savename,
+    #     monitor='val_accuracy', 
+    #     verbose=0, 
+    #     save_best_only=True, 
+    #     mode='auto', 
+    #     save_freq='epoch')
 
-    autoencoder_train = autoencoder.fit(x_train, x_train, 
-                                        batch_size=batch_size,
-                                        epochs=epochs,
-                                        verbose=1,
-                                        validation_data=(x_test, x_test))#,
-                                        # callbacks=[mc, tensorboard])
+    # autoencoder_train = autoencoder.fit(x_train, x_train, 
+    #                                     batch_size=batch_size,
+    #                                     epochs=epochs,
+    #                                     verbose=1,
+    #                                     validation_data=(x_test, x_test))#,
+    #                                     # callbacks=[mc, tensorboard])
 
 
 
