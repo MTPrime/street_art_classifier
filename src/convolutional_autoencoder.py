1069fec6e222f9c030fff568b7355a5b249b23ce
MTPrime <michael@tylerclan.com>
2019-11-07 10:58:06 -0700

    Changed autoencoder architecture.

--- src/convolutional_autoencoder.py
+++ src/convolutional_autoencoder.py
@@ -59,15 +59,15 @@ def autoencoder(input_img):
     #input = 28 x 28 x 1 (wide and thin)
     conv1 = Conv2D(32, (3, 3), activation='relu', padding='same')(input_img) #28 x 28 x 32
     pool1 = MaxPooling2D(pool_size=(2, 2))(conv1) #14 x 14 x 32
-    conv2 = Conv2D(64, (3, 3), activation='relu', padding='same')(pool1) #14 x 14 x 64
+    conv2 = Conv2D(16, (3, 3), activation='relu', padding='same')(pool1) #14 x 14 x 64
     pool2 = MaxPooling2D(pool_size=(2, 2))(conv2) #7 x 7 x 64
-    conv3 = Conv2D(128, (3, 3), activation='relu', padding='same')(pool2) #7 x 7 x 128 (small and thick)
-    code_layer = MaxPooling2D((2, 2), padding='same')(conv3)
+    conv3 = Conv2D(8, (3, 3), activation='relu', padding='same')(pool2) #7 x 7 x 128 (small and thick)
+    code_layer = MaxPooling2D(pool_size=(2, 2), padding='same')(conv3)
 
     #decoder
-    conv4 = Conv2D(128, (3, 3), activation='relu', padding='same')(code_layer) #7 x 7 x 128
+    conv4 = Conv2D(8, (3, 3), activation='relu', padding='same')(code_layer) #7 x 7 x 128
     up1 = UpSampling2D((2,2))(conv4) # 14 x 14 x 128
-    conv5 = Conv2D(64, (3, 3), activation='relu', padding='same')(up1) # 14 x 14 x 64
+    conv5 = Conv2D(16, (3, 3), activation='relu', padding='same')(up1) # 14 x 14 x 64
     up2 = UpSampling2D((2,2))(conv5) # 28 x 28 x 64
     decoded = Conv2D(3, (3, 3), activation='sigmoid', padding='same')(up2) # 28 x 28 x 1
     return decoded
