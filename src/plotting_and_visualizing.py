9a2abca78b38670fae9ca9ab425b59c2ff9f5457
MTPrime <michael@tylerclan.com>
2019-11-11 14:55:34 -0700

    Added new image prediction and plotting

--- src/plotting_and_visualizing.py
+++ src/plotting_and_visualizing.py
@@ -11,6 +11,9 @@ from skimage import color, transform, restoration, io, feature, filters
 from sklearn.metrics import confusion_matrix
 import numpy as np
 import seaborn as sns
+import numpy as np 
+import glob
+
 
 #Needs to be run in tensorflow docker from notebook. The code below changes the file path to allow for this.
 
@@ -104,13 +107,14 @@ class CNNPlotting():
 
         plt.show()
 
-    def plot_incorrect(self, indx, num_classes, image_size=(150,150)):
+    def plot_incorrect(self, indx, num_classes, image_size=(100,100)):
         self.generator.reset()
-        class_names = list(self.generator.class_indices.keys())
+        self.class_names = sorted(list(self.generator.class_indices.keys()))
         class_dict = {v: k for k, v in self.generator.class_indices.items()}
 
         for i in range(num_classes):
-            print(class_names[i].capitalize() + ': ' + str(self.yhat[self.y_incorrect][indx][i]))
+            print("{} : {:.2f}".format(self.class_names[i].capitalize(), self.yhat[self.y_incorrect][indx][i]))
+
         actual = self.y[self.y_incorrect][indx]
         print("Actual - " + class_dict[actual].capitalize())
         
@@ -128,20 +132,64 @@ class CNNPlotting():
         #Displays the full image
         self.display_image_in_actual_size(files[self.y_incorrect][indx])
     
+    def resize(self, file):
+        img = io.imread(file)
+        out = transform.resize(img, (100, 100), anti_aliasing=True)
+        return out
+
+    def classify_new_image(self, img):
+        formatted_img = self.resize(img)
+        yhat = self.model.predict(formatted_img.reshape(-1,100,100,3))
+        for i in range(len(self.class_names)):
+            print("{} : {:.2f}".format(self.class_names[i].capitalize(), yhat[0][i]))
+        raw_img = io.imread(img)
+        fig, ax = plt.subplots()
+        ax.set_xticks([])
+        ax.set_yticks([])
+        plt.imshow(raw_img)
+
+def plot_avg_picture(folder_names, img_cols, img_rows):
+    """
+    Created interesting images when combining only a few images. 
+    With entire classes it turns into a grey blob. Not useful in final EDA
+    """
+    # picture_list = []
+    picture_list = [file for file in glob.glob(folder_names + "/*")]
+    # for file in glob.glob(folder_names + "/*"): 
+    #     picture_list.append(io.imread(file))
+
+    # Assuming all images are the same size, get dimensions of first image
+    w = img_cols 
+    h = img_rows
+    N=len(picture_list)
+
+    # Create a numpy array of floats to store the average (assume RGB images)
+    arr=np.zeros((h,w,3),np.float)
+
+    # Build up average pixel intensities, casting each image as an array of floats
+    for img in picture_list:
+        imarr=np.array(io.imread(img),dtype=np.float)
+        arr=arr+imarr/N
+
+    # Round values in array and cast as 8-bit integer
+    arr=np.array(np.round(arr),dtype=np.uint8)
+
+    # Generate, save and preview final image
+    plt.imshow(arr,cmap="RGB")
 
 if __name__ =='__main__':
     batch_size = 16
-    img_rows, img_cols = 150, 150
+    img_rows, img_cols = 100, 100
 
-    train_generator, test_generator, val_generator = create_data_generators(directory_path='../data/train_test_split/', 
+    train_generator, test_generator, val_generator = create_data_generators(directory_path='data/train_test_split/', 
                                                                         input_shape=(img_rows,img_cols), 
                                                                         batch_size=batch_size)
 
-    model = load_model('../models/3_epoch_model_150_87.h5')
+    model = load_model('models/5_class_model_best.h5')
 
     art_plotting = CNNPlotting(model, val_generator)
 
-    art_plotting.plot_incorrect(indx=75, num_classes=2, image_size=(img_rows,img_cols))
-    
-    class_labels = ['3D', 'Brush', 'Cartoon', 'Realistic', 'Wildstyle']
+    art_plotting.plot_incorrect(indx=75, num_classes=5, image_size=(img_rows,img_cols))
+
+    class_labels = ['Bubble', 'Cartoon', 'Realistic', 'Stencil', 'Wildstyle']
     art_plotting.plot_confusion_matrix(class_labels)
