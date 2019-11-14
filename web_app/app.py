435afa40d8e3594a22a8d34869c098d3bfcc2058
MTPrime <michael@tylerclan.com>
2019-11-13 22:43:56 -0700

    Tweaking app

--- web_app/app.py
+++ web_app/app.py
@@ -82,10 +82,10 @@ def upload():
         file_path = os.path.join(UPLOADPATH, secure_filename(f.filename))
         f.save(file_path)
         rotate_save(f, file_path)
-        predictions = classify_new_image(os.path.split(file_path)[1], classifier)
+        # predictions = classify_new_image(os.path.split(file_path)[1], classifier)
         return redirect(url_for('uploaded_file',
-                        filename=os.path.split(file_path)[1],
-                        predictions=predictions))
+                        filename=os.path.split(file_path)[1]))#,
+                        # predictions=predictions))
     if len(os.listdir(UPLOADPATH)) != 0:
         for file in os.listdir(UPLOADPATH):
             os.remove(os.path.join(UPLOADPATH, file))
@@ -93,8 +93,8 @@ def upload():
 
 
 @app.route('/show/<filename>')
-def uploaded_file(filename, predictions):
-    return render_template('predictions.html', filename=filename, predictions=predictions)
+def uploaded_file(filename):#, predictions):
+    return render_template('predictions.html', filename=filename)#, predictions=predictions)
 
 @app.route('/uploads/<filename>')
 def send_file(filename):
