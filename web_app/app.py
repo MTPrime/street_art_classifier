3b2dd775b5f565926301b953220c8bc69d849aa6
MTPrime <michael@tylerclan.com>
2019-11-13 21:19:49 -0700

    Flask App

---
+++ web_app/app.py
@@ -0,0 +1,104 @@
+import flask 
+from models import * 
+from flask import Flask, redirect, render_template, request, url_for
+from PIL import Image, ExifTags
+
+from flask import Flask, render_template, flash, request, redirect, url_for, send_from_directory
+import sys
+import os
+from werkzeug.utils import secure_filename
+from PIL import Image, ExifTags
+import numpy as np
+from gevent.pywsgi import WSGIServer
+import pandas as pd
+
+FILEPATH = os.path.realpath(__file__)
+ROOTPATH = os.path.split(FILEPATH)[0]
+UPLOADPATH = os.path.join(ROOTPATH, 'uploads')
+
+app = Flask(__name__)
+
+# encoder_model='data/best_encoder_decoder.h5'
+# classifier_model='data/5_class_model_best.h5'
+# classifier = load_model(classifier_model)
+# autoencoder = load_model(encoder_model) 
+
+def rotate_save(f, file_path):
+    try:
+        image = Image.open(f)
+        for orientation in ExifTags.TAGS.keys():
+            if ExifTags.TAGS[orientation] == 'Orientation':
+                break
+        exif = dict(image._getexif().items())
+
+        if exif[orientation] == 3:
+            image = image.rotate(180, expand=True)
+        elif exif[orientation] == 6:
+            image = image.rotate(270, expand=True)
+        elif exif[orientation] == 8:
+            image = image.rotate(90, expand=True)
+        image.save(file_path)
+        image.close()
+
+    except (AttributeError, KeyError, IndexError):
+        image.save(file_path)
+        image.close()
+
+
+@app.route('/', methods=['GET'])
+def index():
+    """Render a simple splash page."""
+    return render_template('index.html')
+    
+# def hello_world():
+#     return'''
+#         <h2> Welcome to my Webpage.</h2>
+#         <form action="/predict" method='POST'>
+#             <input type="text" name="user_input" />
+#             <input type="submit" />
+#         </form>
+#         <form action="/" >
+#             <input type="submit" value = "Go to the homepage."/>
+#         </form>
+#     '''
+
+@app.route('/styles',methods=['GET', 'POST'])
+def styles():
+    return render_template('styles.html')
+
+@app.route('/about',methods=['GET', 'POST'])
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+@app.route('/predictions', methods=["GET", "POST"])
+def upload():
+    if request.method == 'POST':
+        f = request.files["file"]
+        file_path = os.path.join(UPLOADPATH, secure_filename(f.filename))
+        f.save(file_path)
+        # rotate_save(f, file_path)
+        # model_predict(file_path, CURRENTMODEL)
+        return redirect(url_for('uploaded_file',
+                        filename=os.path.split(file_path)[1]))
+    if len(os.listdir(UPLOADPATH)) != 0:
+        for file in os.listdir(UPLOADPATH):
+            os.remove(os.path.join(UPLOADPATH, file))
+    return render_template('predictions.html')
+
+
+@app.route('/show/<filename>')
+def uploaded_file(filename):
+    return render_template('predictions.html', filename=filename)
+
+@app.route('/uploads/<filename>')
+def send_file(filename):
+    return send_from_directory(UPLOADPATH, filename)
+
+if __name__ == '__main__':
+
+    app.run(app.run(host='0.0.0.0', port=5000, threaded=True))
---
+++ web_app/models.py
@@ -0,0 +1,71 @@
+import pickle
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from skimage import io, transform
+from sklearn.metrics.pairwise import cosine_similarity
+
+# from tensorflow import keras
+# from tensorflow.keras.models import load_model
+
+
+def format_image(file_path):
+    """
+    Reads in an image from a file path and resizes it to be 100 x 100.
+    Returns formatted image of the correct shape.
+    """
+    img = io.imread(file_path)
+    out = transform.resize(img, (100, 100), anti_aliasing=True)
+    return out
+
+def process_new_image(img, autoencoder):
+    formatted = format_image(img)
+    encoding = autoencoder.get_layer('encoder').predict(formatted.reshape(1, 100,100,3))
+    return encoding
+
+def make_recommendations(img_file_path, autoencoder, df_filepath='data/encoded_dataframe.csv'):
+    df = pd.read_csv(df_filepath)
+    files = df.pop('file_path')
+    encoded_np = df.to_numpy()
+    encoded_img = process_new_image(img_file_path, autoencoder)
+    recommendations = cosine_similarity(encoded_img.reshape(1,-1), encoded_np)
+    sorted_recommendations = np.argsort(recommendations)
+
+    #Slice reverses the array to put the most similar at the start and then takes the top 5.
+    top_5 = sorted_recommendations[0][:-6:-1] 
+    #Retrieve the file path for 10 closest images
+    file_paths = files.iloc[top_5]
+    recommended_images = [format_image(i) for i in file_paths]
+
+    fig, axs = plt.subplots(1,5, figsize=(20,20))
+    fig.suptitle("Recommended Images", fontsize=36, y=.63)
+    plt.tight_layout()
+    for i, ax in enumerate(axs.flat):
+        ax.grid(False)
+        ax.axis('off')
+        ax.imshow(recommended_images[i])
+    plt.show()
+
+def classify_new_image(img, model):
+    """
+    Used to classify new images.
+    """
+
+    formatted_img = format_image(img)
+    class_names = sorted(list(model.class_indices.keys()))
+    yhat = model.predict(formatted_img.reshape(-1,100,100,3))
+    for i in range(len(class_names)):
+        print("{} : {:.2f}".format(class_names[i].capitalize(), yhat[0][i]))
+    raw_img = io.imread(img)
+    fig, ax = plt.subplots()
+    ax.set_xticks([])
+    ax.set_yticks([])
+    plt.imshow(raw_img)
+
+if __name__ == '__main__':
+    pass
+    # encoder_model='data/best_encoder_decoder.h5'
+    # classifier_model='data/5_class_model_best.h5'
+    # classifier = load_model(classifier_model)
+    # autoencoder = load_model(encoder_model) 
